3442eab51f59802a365e96bd50f647dab0cece55
Iqbalnrflah <iqbalnurfalah2501@gmail.com>
2025-12-13 23:35:55 +0700

    Fitur Login

--- lib/pages/welcome_pages.dart
+++ lib/pages/welcome_pages.dart
@@ -18,18 +18,70 @@ class WelcomePages extends StatelessWidget {
           Text(
             "Welcome",
             style: dangerTextStyle,
+            textAlign: TextAlign.center,
           ),
           SizedBox(
             height: 15,
           ),
           Text(
-            "lorem ipsum",
+            "Hello my name is iqbal",
             style: whiteTextStyle.copyWith(fontSize: 28),
+            textAlign: TextAlign.center,
           ),
           SizedBox(
             height: 51,
           ),
-        ],
+          Container(
+            height: 60,
+            width: MediaQuery.of(contex).size.width,
+            child : ElevatedButton(
+              onPressed: (){}, 
+              child: Text(
+                'Create Account', 
+                style:whiteTextStyle.copyWith(
+                  fontSize: 20, 
+                  fontWeight: FontWeight.w300, 
+                  color: primary),
+                  ), style: ElevatedButton.styleFrom(
+                    backgroundColor: secondaryColor, 
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(25)))
+              )
+          ),
+          SizedBox(
+            height: 15,
+          ),
+          Container(
+            height: 60,
+            width: MediaQuery.of(contex).size.width,
+            child : ElevatedButton(
+              onPressed: (){}, 
+              child: Text(
+                'Loginn', 
+                style:whiteTextStyle.copyWith(
+                  fontSize: 20, 
+                  fontWeight: FontWeight.w300, 
+                  color: secondaryColor),
+                  ), 
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: primary, 
+                    shape: RoundedRectangleBorder(
+                      side: BorderSide(color: secondaryColor, width: 3),
+                      borderRadius: BorderRadius.circular(25))))),
+          SizedBox(
+            height: 30,
+          ),
+          Text(
+          'All Right Reserved @2025',
+          textAlign: TextAlign.center,
+          style: 
+          whiteTextStyle.copyWith(
+            color: secondaryColor, fontSize: 11),
+            ),
+          SizedBox(
+            height: defaultMargin,
+          ),
+        ]
       ),),
 
     );
